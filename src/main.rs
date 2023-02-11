08aca2547801c7e47b76f413bb2e62d231ef20a1
Andross2200 <andre.shamanayev@gmail.com>
2023-02-11 14:02:08 +0100

    Figure out version of ggez to use

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+//! The simplest possible example that does something.
+#![allow(clippy::unnecessary_wraps)]
+
 use ggez::{
     event,
     glam::*,
@@ -18,24 +21,24 @@ impl MainState {
             vec2(0., 0.),
             100.0,
             2.0,
-            Color::BLACK,
+            Color::WHITE,
         )?;
 
-        Ok(MainState { pos_x: 50.0, circle })
+        Ok(MainState { pos_x: 0.0, circle })
     }
 }
 
 impl event::EventHandler<ggez::GameError> for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        self.pos_x = self.pos_x + 1.0;
+        self.pos_x = self.pos_x % 800.0 + 1.0;
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas =
-            graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.5, 0.3, 1.0]));
+            graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
 
-        canvas.draw(&self.circle, Vec2::new(self.pos_x, 180.0));
+        canvas.draw(&self.circle, Vec2::new(self.pos_x, 380.0));
 
         canvas.finish(ctx)?;
 
